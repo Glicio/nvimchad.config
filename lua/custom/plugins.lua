bb599d66f344aba54a727decbb51dc8dfc2eec82
Glicio Pereira Uchoa <glicioo@outlook.com>
2024-05-23 11:16:04 -0300

    fixed copilot keymap

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -22,10 +22,11 @@ local plugins ={
     "github/copilot.vim",
     lazy = false,
     config = function()
-      vim.keymap.set("i","<C-g>",'copilot#Accept()', {
-        expr = true,
-        replace_keycodes = true,
-      })
+        vim.keymap.set('i', '<C-g>', 'copilot#Accept("\\<CR>")', {
+          expr = true,
+          replace_keycodes = false
+        })
+        vim.g.copilot_no_tab_map = true
       vim.keymap.set("i","<C-n>",'copilot#Next()', {
         expr = true,
         replace_keycodes = true,
