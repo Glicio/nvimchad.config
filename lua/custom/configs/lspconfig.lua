7469b12a2f3e87e0684f3c41d1b8a153148ff941
Glicio Pereira Uchoa <glicioo@outlook.com>
2024-12-11 14:57:21 -0300

    added sql

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = base.capabilities
 
 local lspconfig = require("lspconfig")
 
-local servers = {"bashls", "html", "cssls", "ts_ls", "prismals", "tailwindcss", "emmet_ls", "gradle_ls", "gopls", "jdtls" }
+local servers = {"bashls", "html", "cssls", "ts_ls", "prismals", "tailwindcss", "emmet_ls", "gradle_ls", "gopls", "jdtls", "sqlls" }
 
 for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup {
